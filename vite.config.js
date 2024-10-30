425dfbd49ba25957e7c722bcb7a56643c2ff5352
HoangHan594 <nguyenhoanghan2003@gmail.com>
2024-10-30 22:31:17 +0700

    khoi tao

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
         port: 3001,
         proxy: {
             "/api": {
-                target: "https://luanvan-fe.vercel.app/",
+                target: "http://localhost:3000/",
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, ''),
             },
