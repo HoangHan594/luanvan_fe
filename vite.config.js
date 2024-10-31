c7ac30bec119f4d29b170015956a39a0f4149891
HoangHan594 <nhathoang19012003@gmail.com>
2024-10-31 15:46:53 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
         port: 3001,
         proxy: {
             "/api": {
-                target: "http://localhost:3000/",
+                target: "https://luanvan-1-kmlh.onrender.com",
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, ''),
             },
