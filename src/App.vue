1c2f91e5c7feb538c95801262310a6e43b8978af
HoangHan594 <nhathoang19012003@gmail.com>
2024-10-31 13:04:57 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<!-- <template>
+  <div>
+    <AppHeader /> 
+    <div id="app">
+      <router-view />
+      là một thẻ placeholder mà Vue Router sử dụng để render nội dung của các route trong ứng dụng Vue.js của bạn. 
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+};
+</script> -->
 <template>
   <div>
     <!-- <AppHeader /> -->
@@ -9,9 +26,33 @@
 </template>
 
 <script>
-
 export default {
-  components: {
+  data() {
+    return {
+      dbStatus: 'unknown',
+    };
+  },
+  methods: {
+    async checkDatabaseConnection() {
+      try {
+        const response = await fetch('https://luanvan-1-kmlh.onrender.com');
+        const data = await response.json();
+
+        if (data.database === 'connected') {
+          this.dbStatus = 'Connected';
+          console.log('Connected to database');
+        } else {
+          this.dbStatus = 'Not Connected';
+          console.warn('Database is not connected');
+        }
+      } catch (error) {
+        this.dbStatus = 'Error';
+        console.error('Error connecting to database:', error);
+      }
+    },
+  },
+  mounted() {
+    this.checkDatabaseConnection();
   },
 };
 </script>
