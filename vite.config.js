8d4c718c78cc85b6bf0274247213ccf3f132b338
HoangHan594 <nhathoang19012003@gmail.com>
2024-11-01 21:27:55 +0700

    last commit agian +16

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,17 @@ export default defineConfig({
     define: {
         'process.env.API_TARGET': JSON.stringify(apiTarget), // Truyền API Target vào môi trường
     },
-    base: isProduction ? './' : '/', // Thay đổi base cho môi trường sản xuất
+    base: './', // Thay đổi base cho môi trường sản xuất
     plugins: [
         vue(),
-        vueJsx(),
-        Components({
-            resolvers: [
-                AntDesignVueResolver({
-                    importStyle: false,
-                }),
-            ],
-        }),
+        // vueJsx(),
+        // Components({
+        //     resolvers: [
+        //         AntDesignVueResolver({
+        //             importStyle: false,
+        //         }),
+        //     ],
+        // }),
     ],
     resolve: {
         alias: {
@@ -40,7 +40,7 @@ export default defineConfig({
         port: 3001,
         proxy: {
             "/api": {
-                target: 'https://luanvan-1-kmlh.onrender.com/', // Sử dụng target được xác định trước
+                target: apiTarget, // Sử dụng target được xác định trước
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, ''),
             },
