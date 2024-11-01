7d1e87047e324fc278b38b311200531e0dfa653c
HoangHan594 <nhathoang19012003@gmail.com>
2024-11-01 22:19:08 +0700

    last commit agian +19

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,19 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+// Kiểm tra chế độ môi trường
+const isProduction = process.env.BUILD_MODE === 'production';
+
+// Xác định target dựa trên chế độ
+const apiTarget = isProduction ? 'https://luanvan-1-kmlh.onrender.com' : 'http://localhost:3000';
+
+// In ra giá trị của apiTarget
+console.log('API Target:', apiTarget);
 
 export default defineConfig({
-    // define: {
-    //     'process.env.API_TARGET': JSON.stringify(apiTarget), // Truyền API Target vào môi trường
-    // },
+    define: {
+        'process.env.API_TARGET': JSON.stringify(apiTarget), // Truyền API Target vào môi trường
+    },
     base: './', // Thay đổi base cho môi trường sản xuất
     plugins: [
         vue(),
@@ -32,7 +40,7 @@ export default defineConfig({
         port: 3001,
         proxy: {
             "/api": {
-                target: 'https://luanvan-1-kmlh.onrender.com', // Sử dụng target được xác định trước
+                target: apiTarget, // Sử dụng target được xác định trước
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, ''),
             },
